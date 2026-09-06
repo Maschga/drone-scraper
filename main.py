9e912656ffe76c5e367fcd0b5f77cadd9c614cf8
Maschga <delphi@freenet.de>
2026-09-06 09:31:14 +0200

    make public

--- main.py
+++ main.py
@@ -672,7 +672,7 @@ async def api_collect():
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=False,
     )
